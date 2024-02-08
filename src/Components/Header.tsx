6ef59738222a6500fd4fb324cd693475448935bb
rodpru <rgp.prt@gmail.com>
2024-02-08 14:28:46 +0000

    wrap up

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,58 +10,15 @@ const Header = () => {
           <Link to="/">
             <img className="w-10 h-10" src={logo} alt="NBA" />
           </Link>
-          <div className="flex md:order-2">
-            <div className="relative mr-3 md:mr-0 hidden md:block">
-              <input
-                type="text"
-                id="email-adress-icon"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2"
-                placeholder="Search..."
-              />
-            </div>
-            <button
-              data-collapse-toggle="mobile-menu-3"
-              type="button"
-              className="md:hidden text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center"
-              aria-controls="mobile-menu-3"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <svg
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
           <div
-            className="hidden md:flex justify-between items-center w-full md:w-auto md:order-1"
+            //className="md:flex justify-between items-center w-full md:w-auto md:order-1"
             id="mobile-menu-3"
           >
-            <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <ul className="flex-row flex space-x-8 mt-0 text-sm font-medium">
               <li>
                 <Link
                   to="/"
-                  className="text-gray-700 hover:text-red-700 bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded"
+                  className="hover:text-red-700 bg-transparent block pl-3 pr-4 py-2 text-blue-700 md:p-0 rounded"
                 >
                   Home
                 </Link>
